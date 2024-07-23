d235b34486ec47dfd6abd88b456e26ebc35bdc99
pc <pc@DESKTOP-M7QJQNL>
2024-07-23 11:15:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+
+
+Console.WriteLine("Kaç defa yazsın program?");
+int limit = Convert.ToInt32(Console.ReadLine());
+int sayac = 0;
+
+while (sayac < limit)
+{
+    Console.WriteLine("PATİKE");
+    sayac++;
+}
+
+
+
+
+
+Console.WriteLine("Kaç defa yazsın program?");
+int limit = Convert.ToInt32(Console.ReadLine());
+int sayac = 0;
+
+do
+{
+    Console.WriteLine("PATİKE");
+    sayac++;
+}
+
+while (sayac < limit);
+
+
+// do-while da komut 1 kere mutlaka çalıştırılır.
